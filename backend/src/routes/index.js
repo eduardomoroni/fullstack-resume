74e2d6dcacbd950a4a750926a13dbac5e89a7c0d
Eduardo Moroni <eduardomoroni@gmail.com>
2018-03-22 18:12:20 -0300

    refactor: router

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,16 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const createError = require('http-errors');
+const { Router } = require('express');
+
+const router = Router();
 
 router.get('/emprego', function(req, res, next) {
-  res.render('index', { title: 'Emprego' });
+  res.status(200).json({ title: 'Emprego' });
 });
 
 router.get('/tempo', function(req, res, next) {
-  res.render('index', { title: 'Tempo' });
+  res.status(200).json({ title: 'Tempo' });
 });
 
 router.get('/hello', function(req, res, next) {
-  res.render('index', { title: 'Hello' });
+  res.status(200).json({ title: 'Hello' });
 });
 
-module.exports = router;
+router.use(function(req, res, next) {
+  next(createError(404));
+})
+
+router.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+})
+
+module.exports = { router }
