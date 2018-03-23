17497808e68c1123a39ee74cef4d3796b2a6998e
Eduardo Moroni <eduardomoroni@gmail.com>
2018-03-23 20:09:58 -0300

    feat: enable CORS during development

--- backend/src/presentation/api.js
+++ backend/src/presentation/api.js
@@ -1,11 +1,16 @@
 const logger = require('morgan');
 const express = require('express');
 const { router } = require('./routes');
+const { enableCORS } = require('./middlewares/cors');
 
 const app = express();
 
 app.disable('etag');
 app.disable('x-powered-by');
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(enableCORS);
+}
 
 app.use(logger('dev'));
 app.use(express.json());
