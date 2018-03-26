773bb677ccdcb83d0e54548441db63bd7d15c604
Eduardo Moroni <eduardomoroni@gmail.com>
2018-03-26 11:16:47 -0300

    refactor: exchange manual middleware by express middleware

--- backend/src/presentation/api.js
+++ backend/src/presentation/api.js
@@ -1,15 +1,15 @@
 const logger = require('morgan');
 const express = require('express');
 const { router } = require('./routes');
-const { enableCORS } = require('./middlewares/cors');
+const CORS = require('cors');
 
 const app = express();
 
 app.disable('etag');
 app.disable('x-powered-by');
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(enableCORS);
+if (process.env.NODE_ENV !== 'production') {
+  app.use(CORS());
 }
 
 app.use(logger('dev'));
